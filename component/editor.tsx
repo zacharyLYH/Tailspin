ffe98e21cba2c7cf8901562928fe5bc2d460ca0d
Zac <yihongle@buffalo.edu>
2023-08-31 09:38:44 +0800

    basic editor with tailwind and nice editor features

---
+++ component/editor.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { useState, useEffect, Dispatch, SetStateAction } from "react";
+
+interface EditorProps {
+    code: string;
+    setCode: Dispatch<SetStateAction<string>>;
+}
+
+const Editor: React.FC<EditorProps> = ({ code, setCode }) => {
+    const [AceEditor, setAceEditor] = useState<any>(null);
+
+    useEffect(() => {
+        const loadAce = async () => {
+            // dynamically load the Ace Editor component
+            const aceEditorModule = await import("react-ace");
+            const AceEditor = aceEditorModule.default;
+
+            // dynamically load modes and themes
+            await import("ace-builds/src-noconflict/mode-html");
+            await import("ace-builds/src-noconflict/theme-monokai");
+            await import("ace-builds/src-noconflict/ext-language_tools");
+
+            // set Ace Editor component state
+            setAceEditor(() => AceEditor);
+        };
+
+        loadAce();
+    }, []);
+
+    if (!AceEditor) return <div>Loading...</div>;
+
+    return (
+        <AceEditor
+            placeholder="Placeholder Text"
+            mode="html"
+            theme="monokai"
+            name="editor"
+            onChange={(newCode: string) => setCode(newCode)}
+            fontSize={14}
+            showPrintMargin={true}
+            showGutter={true}
+            highlightActiveLine={true}
+            value={code}
+            setOptions={{
+                enableBasicAutocompletion: true,
+                enableLiveAutocompletion: true,
+                enableSnippets: true,
+                showLineNumbers: true,
+                tabSize: 2,
+            }}
+        />
+    );
+};
+
+export default Editor;
---
+++ component/preview.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+interface PreviewProps {
+    code: string;
+}
+
+{
+    /* <div class='bg-blue-500'>Hello</div>
+<h1 class="text-slate-200">Hola</h1> */
+}
+
+const Preview: React.FC<PreviewProps> = ({ code }) => {
+    const completedCode = `<!doctype html>
+    <html>
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+    <body>
+      ${code}
+    </body>
+    </html>`;
+    return (
+        <iframe
+            title="Preview"
+            srcDoc={completedCode}
+            // other options
+        />
+    );
+};
+
+export default Preview;
