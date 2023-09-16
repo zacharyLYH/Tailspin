93f661f971695269436cd8c22353c2e2b060dfa5
Zac <yihongle@buffalo.edu>
2023-09-16 22:22:16 +0800

    placeholder for tutorial

--- components/self/landing/tabs.tsx
+++ components/self/landing/tabs.tsx
@@ -1,25 +1,33 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Image as LucideImg, Code } from "lucide-react";
+import { Image as LucideImg, Code, GraduationCap } from "lucide-react";
 import Editor from "@/components/self/editor";
 import Preview from "@/components/self/preview";
 import { StaticImage } from "../challengeImage/static";
 import { BrowserMockup } from "@/components/ui/browserMockup";
+import { FeedbackModal } from "@/components/self/landing/feedback/feedback-modal";
 
 export const TabSection = () => {
     return (
         <div className="h-screen" id="tabsSection">
-            <h1 className="text-green-300 text-center">
-                Tailspin is still in development! Give us feedback here.
+            <h1 className="text-green-500 text-center font-semibold italic">
+                🔈 Tailspin is still in development! Click on the face{" "}
+                <FeedbackModal /> and help make Tailspin great!
             </h1>
-            <Tabs defaultValue="image" className="w-full mt-4">
+            <Tabs defaultValue="tutorial" className="w-full mt-4">
                 <TabsList className="flex mx-64">
-                    <TabsTrigger className="w-1/2" value="image">
+                    <TabsTrigger className="w-1/3" value="tutorial">
+                        Quick Start <GraduationCap className="w-4 h-4 ml-1" />
+                    </TabsTrigger>
+                    <TabsTrigger className="w-1/3" value="image">
                         Sample Image <LucideImg className="w-4 h-4 ml-1" />
                     </TabsTrigger>
-                    <TabsTrigger className="w-1/2" value="code">
+                    <TabsTrigger className="w-1/3" value="code">
                         Code Area <Code className="w-4 h-4 ml-1" />
                     </TabsTrigger>
                 </TabsList>
+                <TabsContent value="tutorial" className="h-[90vh]">
+                    <p>Tutorial placeholder</p>
+                </TabsContent>
                 <TabsContent value="image" className="h-[90vh]">
                     <BrowserMockup>
                         <StaticImage challengeImageURL="/vercel.svg" />
