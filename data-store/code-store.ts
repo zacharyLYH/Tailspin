0ebb6bf56529c71f8531e0f4b62dfb5a1ced9249
Zac <yihongle@buffalo.edu>
2023-09-16 22:38:54 +0800

    modified initial code

--- data-store/code-store.ts
+++ data-store/code-store.ts
@@ -7,7 +7,17 @@ interface CodeStore {
 }
 
 const useCodeStore = create<CodeStore>((set) => ({
-    code: "<div class='bg-blue-500'>Hello</div>",
+    code: `<!--Use the code formatter 👆 to neaten your code-->
+
+    <!--You'll write code in this code editor-->
+    <div class="flex items-center justify-center h-screen">
+      <p class="text-2xl font-semibold relative z-10 shadow-lg rounded-full p-2">
+        👈 Start writing HTML and Tailwind in the
+        <span class="text-green-500">Editor</span>
+      </p>
+    </div>
+    
+    <!--See the results of your code in the preview on the right 👉-->`,
     setCode: (newCode) => set({ code: newCode }),
 }));
 
