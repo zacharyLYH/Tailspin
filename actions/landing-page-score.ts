b32a67023c8c9786ef4984c633ad651ffced46f1
Zac <yihongle@buffalo.edu>
2023-09-17 18:25:04 +0800

    pixel by pixel comparison algorithm. not good

--- actions/landing-page-score.ts
+++ actions/landing-page-score.ts
@@ -2,25 +2,87 @@ import useSessionStore from "@/data-store/session-store";
 import { ConvertPromptStatic } from "@/lib/convert-prompt-static";
 import { captureIframe } from "@/lib/static-screenshot";
 import toast from "react-hot-toast";
+import pixelmatch from "pixelmatch";
+import LandingPageCode from "@/components/self/landing/test-challenges/code";
 
 export const LandingPageScorer = async () => {
-    const { userImg, imgPrompt, setLikenessScore } = useSessionStore.getState();
     try {
         captureIframe(false);
-        ConvertPromptStatic("/button");
-        const resp = await fetch("/api/score/static", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ img1: imgPrompt, img2: userImg }),
+        ConvertPromptStatic("/button.png");
+        // Wrap the asynchronous logic in an async function
+        const compareImages = async (userImg: string, imgPrompt: string) => {
+            if (userImg === "" || imgPrompt === "") return;
+
+            const img1 = new Image();
+            const img2 = new Image();
+            img1.src = userImg;
+            img2.src = imgPrompt;
+
+            await Promise.all([
+                new Promise<void>((resolve) => (img1.onload = () => resolve())),
+                new Promise<void>((resolve) => (img2.onload = () => resolve())),
+            ]);
+
+            const canvas1 = document.createElement("canvas");
+            const canvas2 = document.createElement("canvas");
+            canvas1.width = img1.width;
+            canvas1.height = img1.height;
+            canvas2.width = img2.width;
+            canvas2.height = img2.height;
+
+            const ctx1 = canvas1.getContext("2d")!;
+            const ctx2 = canvas2.getContext("2d")!;
+            ctx1.drawImage(img1, 0, 0);
+            ctx2.drawImage(img2, 0, 0);
+
+            const imgData1 = ctx1.getImageData(0, 0, img1.width, img1.height);
+            const imgData2 = ctx2.getImageData(0, 0, img2.width, img2.height);
+
+            if (
+                imgData1.width !== imgData2.width ||
+                imgData1.height !== imgData2.height
+            ) {
+                console.error(
+                    "Images must have the same dimensions for comparison."
+                );
+                return;
+            }
+
+            const diffPixels = new Uint8ClampedArray(imgData1.data.length);
+            const numDiffPixels = pixelmatch(
+                imgData1.data,
+                imgData2.data,
+                diffPixels,
+                imgData1.width,
+                imgData1.height,
+                { threshold: 0.1 }
+            );
+
+            const totalPixels = imgData1.width * imgData1.height;
+            const similarity =
+                ((totalPixels - numDiffPixels) / totalPixels) * 100;
+            toast.success(`${similarity}% likeness`);
+        };
+
+        let justReset = false;
+        useSessionStore.subscribe(async (state) => {
+            const { userImg, imgPrompt, reset, setCode } = state;
+            if (justReset) {
+                justReset = false;
+                return;
+            }
+            if (!userImg || !imgPrompt) {
+                return;
+            }
+            try {
+                await compareImages(userImg, imgPrompt);
+                justReset = true;
+                reset();
+                setCode(LandingPageCode());
+            } catch (err) {
+                console.error("An error occurred while comparing images:", err);
+            }
         });
-        if (resp.status !== 200) {
-            toast.error(`Error code in landing-page-score: ${resp.status} `);
-        } else {
-            toast.success(`Successfully scored!`);
-            setLikenessScore("99%");
-        }
     } catch (error) {
         toast.error(`Something went wrong`);
     }
