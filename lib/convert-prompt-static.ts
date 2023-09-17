fbca19ccbaf728b1790036b4fc0ef9eb6bba93aa
Zac <yihongle@buffalo.edu>
2023-09-17 18:24:24 +0800

    ensure the local image is base64

--- lib/convert-prompt-static.ts
+++ lib/convert-prompt-static.ts
@@ -5,12 +5,19 @@ export const ConvertPromptStatic = async (imgURL: string) => {
     // Fetch image from public folder
     const response = await fetch(imgURL);
     const blob = await response.blob();
-
     // Convert blob to Base64
     const reader = new FileReader();
-    reader.readAsDataURL(blob);
     reader.onloadend = () => {
-        const base64data = reader.result;
-        setImgPrompt(base64data as string);
+        if (typeof reader.result === "string") {
+            const base64data = reader.result.split(",")[1]; // Remove any existing MIME type
+            const pngBase64 = `data:image/png;base64,${base64data}`;
+            setImgPrompt(pngBase64);
+        } else {
+            console.error("Could not convert blob to base64 string");
+        }
     };
+    reader.onerror = () => {
+        console.error("An error occurred while reading the blob");
+    };
+    reader.readAsDataURL(blob);
 };
