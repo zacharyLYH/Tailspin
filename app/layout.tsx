14471cb36cd4968440282a08ab28cf7c8f4e5e66
Zac <yihongle@buffalo.edu>
2023-09-28 10:02:19 +0800

    added react query provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ToasterProvider } from "@/components/providers/toast-provider";
+import TanStackProvider from "@/components/providers/TanstackProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,14 +20,16 @@ export default function RootLayout({
     return (
         <html lang='en'>
             <body className={inter.className}>
-                <ThemeProvider
-                    attribute='class'
-                    defaultTheme='dark'
-                    enableSystem
-                >
-                    <ToasterProvider />
-                    {children}
-                </ThemeProvider>
+                <TanStackProvider>
+                    <ThemeProvider
+                        attribute='class'
+                        defaultTheme='dark'
+                        enableSystem
+                    >
+                        <ToasterProvider />
+                        {children}
+                    </ThemeProvider>
+                </TanStackProvider>
             </body>
         </html>
     );
