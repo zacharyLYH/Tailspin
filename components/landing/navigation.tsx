1963330f330f1281739eacf7a6d52035f525dc5f
Zac <yihongle@buffalo.edu>
2023-10-14 17:34:38 +0800

    made nav bar cleaner

--- components/landing/navigation.tsx
+++ components/landing/navigation.tsx
@@ -1,20 +1,37 @@
 import { Button } from "@/components/ui/button";
-import { Shell } from "lucide-react";
-import Link from 'next/link';
+import Link from "next/link";
+import { TailspinLogo } from "../ui/spinning-logo";
 
 // TODO: Add better responsiveness and mobile design, refactor Button into the button component
 
 export const Navigation = () => {
     return (
-        <nav className='sticky top-0 z-[1] flex h-16 items-center bg-[#181818]'>
+        <nav className='sticky top-0 z-[1] flex h-16 items-center'>
             <Link href='#landing'>
-                <Shell className='mx-5 h-full w-10 text-green-500'/>
+                <div className='mx-5'>
+                    <TailspinLogo outershellClassname='animate-spin h-10 w-10' />
+                </div>
             </Link>
-            <Button className='mx-3 bg-green-500'>About</Button>
+            <Button
+                variant='ghost'
+                className='mx-3 font-semibold text-muted-foreground'
+            >
+                About
+            </Button>
             <Link href='#tabsSection'>
-                <Button className='mx-3 bg-green-500'>Code</Button>
+                <Button
+                    variant='ghost'
+                    className='mx-3 font-semibold text-muted-foreground'
+                >
+                    Code
+                </Button>
             </Link>
-            <Button className='mx-3 bg-green-500'>Tutorial</Button>
+            <Button
+                variant='ghost'
+                className='mx-3 font-semibold text-muted-foreground'
+            >
+                Tutorial
+            </Button>
         </nav>
     );
 };
