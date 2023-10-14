518cba7f0a50359ed612068b865fcc3f89ef4612
Zac <yihongle@buffalo.edu>
2023-10-14 17:34:09 +0800

    faq shell

---
+++ components/landing/faq.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion";
+
+const FAQ = () => {
+    return (
+        <div
+            className='flex h-screen items-center justify-center'
+            id='accordion'
+        >
+            <Accordion type='single' collapsible>
+                <AccordionItem value='item-1'>
+                    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                    <AccordionContent>
+                        Yes. It adheres to the WAI-ARIA design pattern.
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value='item-2'>
+                    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                    <AccordionContent>
+                        Yes. It adheres to the WAI-ARIA design pattern.
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
+        </div>
+    );
+};
+
+export default FAQ;
