fd84b903216ee10ab5f7edb1191795e2b620b96f
Zac <yihongle@buffalo.edu>
2023-10-15 08:33:31 +0800

    faq done

--- components/landing/faq.tsx
+++ components/landing/faq.tsx
@@ -6,24 +6,154 @@ import {
     AccordionItem,
     AccordionTrigger,
 } from "@/components/ui/accordion";
+import Link from "next/link";
 
 const FAQ = () => {
     return (
         <div
-            className='flex h-screen items-center justify-center'
+            className='flex min-h-screen min-w-full flex-col items-center justify-center'
             id='accordion'
         >
-            <Accordion type='single' collapsible>
+            <span className='bold text-center text-2xl italic'>FAQ</span>
+            <Accordion type='single' collapsible className='w-96'>
                 <AccordionItem value='item-1'>
-                    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                    <AccordionTrigger>
+                        Sounds like an interesting app... What's the catch?
+                    </AccordionTrigger>
                     <AccordionContent>
-                        Yes. It adheres to the WAI-ARIA design pattern.
+                        This product is community driven, and not developed and
+                        maintained under the comfortable belly of a large
+                        private enterprise. In other words, Tailspin is not a
+                        polished product. Our business model relies heavily on
+                        community input for
+                        <br />
+                        <ul className='list-disc pl-5'>
+                            <li className='mb-2 text-muted-foreground'>
+                                Creating more Target images (images to
+                                recreate).
+                            </li>
+                            <li className='mb-2 text-muted-foreground'>
+                                Enforce Tailspin&apos;s fair-play policy by
+                                helping verify suspicious submissions.
+                            </li>
+                            <li className='mb-2 text-muted-foreground'>
+                                Active development, innovation, and small
+                                donations for small payments for databases,
+                                hosting environments etc.
+                            </li>
+                        </ul>
+                        <br />
+                        Besides these aforementioned limitations, check out the
+                        next accordion to understand how we are an{" "}
+                        <span className='font-bold text-red-500'>
+                            unpolished
+                        </span>{" "}
+                        product.
                     </AccordionContent>
                 </AccordionItem>
                 <AccordionItem value='item-2'>
-                    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                    <AccordionTrigger>
+                        How are submissions graded?
+                    </AccordionTrigger>
                     <AccordionContent>
-                        Yes. It adheres to the WAI-ARIA design pattern.
+                        We mentioned we use GPT3.5 in the{" "}
+                        <Link
+                            href='#about-section'
+                            className='text-blue-500 underline'
+                        >
+                            About
+                        </Link>{" "}
+                        section. We considered a few other methods, such as
+                        <br />
+                        <ul className='list-disc pl-5'>
+                            <li className='mb-2 text-muted-foreground'>
+                                Pixel matching libraries like{" "}
+                                <Link
+                                    href='https://www.npmjs.com/package/pixelmatch'
+                                    className='text-blue-500 underline'
+                                >
+                                    pixelmatch
+                                </Link>
+                            </li>
+                            <li className='mb-2 text-muted-foreground'>
+                                Image-SSIM techniques like{" "}
+                                <Link
+                                    href='https://www.npmjs.com/package/image-ssim'
+                                    className='text-blue-500 underline'
+                                >
+                                    image-ssim
+                                </Link>
+                            </li>
+                            <li className='mb-2 text-muted-foreground'>
+                                Mathematical based approaches like{" "}
+                                <Link
+                                    href='https://en.wikipedia.org/wiki/Image_histogram#:~:text=An%20image%20histogram%20is%20a,tonal%20distribution%20at%20a%20glance.'
+                                    className='text-blue-500 underline'
+                                >
+                                    image histogram
+                                </Link>
+                                .
+                            </li>
+                        </ul>
+                        <br />
+                        However, these techniques all suffer from images with a
+                        lot of common (usually white) background. Namely, since
+                        2 images share a lot of the same white background,
+                        similarity scores suffer from extreme false positives.
+                        The underlying issue is that these algorithms do not
+                        take into account the{" "}
+                        <span className='text-green-500'>
+                            semantic meaning
+                        </span>{" "}
+                        of submissions. The remedy is to use something
+                        intelligent to extract{" "}
+                        <span className='text-red-500'>intention</span> out of
+                        code, and using today's technology, we looked no further
+                        than LLMs.
+                        <br />
+                        This implementation is not great because
+                        <br />
+                        <ul className='list-disc pl-5'>
+                            <li className='mb-2 text-muted-foreground'>
+                                LLM responses are usually non-deterministic.
+                            </li>
+                            <li className='mb-2 text-muted-foreground'>
+                                LLMs are expensive to use.
+                            </li>
+                            <li className='mb-2 text-muted-foreground'>
+                                LLMs are not bullet-proof at doing this. Through
+                                testing, we've found that their responses are
+                                inconsistent and bias towards false positives.
+                            </li>
+                        </ul>
+                        <br />
+                        Nevertheless, these were the tradeoffs made to provide
+                        the best possible experience with the least amount of
+                        resources, the rest is up to the community. Moving
+                        forward, a possible direction might be to bake an
+                        in-house Computer Vision software that we'll host
+                        in-house, that way we get the benefits of keeping costs
+                        low yet having a tunable and customizable intelligent
+                        sematic analysis tool that exactly fits the kind of
+                        images we hope to compare.
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value='item-3'>
+                    <AccordionTrigger>
+                        I'm a noob/average/pro/better-than-you developer. How
+                        can I contribute?
+                    </AccordionTrigger>
+                    <AccordionContent>
+                        First and foremost, thank you for even considering. We
+                        would absolutely love to get in touch with you. Tailspin
+                        is built by the community for the community afterall.
+                        Hit us up at{" "}
+                        <Link
+                            className='text-blue-500 underline'
+                            href='mailto:tailspin.official@gmail.com'
+                        >
+                            tailspin.official@gmail.com
+                        </Link>
                     </AccordionContent>
                 </AccordionItem>
             </Accordion>
