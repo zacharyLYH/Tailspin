012ee564056e9f004514cdd1d30c692c7d1ce37b
Zac <yihongle@buffalo.edu>
2023-10-20 21:18:11 +0800

    remove unused code

--- components/core/editor.tsx
+++ components/core/editor.tsx
@@ -2,21 +2,14 @@
 
 import { Button } from "@/components/ui/button";
 import useSessionStore from "@/data-store/session-store";
-import useToggleFullScreen from "@/data-store/full-screen-store";
 import { useFormatCode } from "@/lib/codeFormatter";
-import { cn } from "@/lib/utils";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import ReactAce from "react-ace/lib/ace";
 import GlobalLoadingUI from "@/components/ui/global-loading-ui";
 
-interface EditorProps {
-    className?: string;
-}
-
-const Editor: React.FC<EditorProps> = ({ className }) => {
+const Editor = () => {
     const { code, setCode } = useSessionStore();
     const { handleFormatCode } = useFormatCode();
-    const { fullScreen } = useToggleFullScreen();
     const [AceEditor, setAceEditor] = useState<typeof ReactAce>();
 
     useEffect(() => {
@@ -30,7 +23,6 @@ const Editor: React.FC<EditorProps> = ({ className }) => {
             await import("ace-builds/src-noconflict/theme-monokai");
             await import("ace-builds/src-noconflict/ext-language_tools");
 
-            // set Ace Editor component state
             setAceEditor(() => AceEditor);
         };
 
@@ -40,7 +32,7 @@ const Editor: React.FC<EditorProps> = ({ className }) => {
     if (!AceEditor) return <GlobalLoadingUI />;
 
     return (
-        <div className={cn("flex flex-col", fullScreen ? "hidden" : className)}>
+        <>
             <div className='flex flex-row space-x-2'>
                 <Button
                     onClick={handleFormatCode}
@@ -49,31 +41,33 @@ const Editor: React.FC<EditorProps> = ({ className }) => {
                     Format code
                 </Button>
             </div>
-            <AceEditor
-                wrapEnabled={true}
-                width='100%'
-                placeholder='Placeholder Text'
-                mode='html'
-                theme='monokai'
-                name='editor'
-                height='100%'
-                onChange={(newCode: string) => setCode(newCode)}
-                fontSize={14}
-                showPrintMargin={true}
-                showGutter={true}
-                highlightActiveLine={true}
-                value={code}
-                setOptions={{
-                    enableBasicAutocompletion: true,
-                    enableLiveAutocompletion: true,
-                    enableSnippets: true,
-                    showLineNumbers: true,
-                    tabSize: 2,
-                    wrapBehavioursEnabled: true,
-                    wrap: true,
-                }}
-            />
-        </div>
+            {AceEditor && (
+                <AceEditor
+                    wrapEnabled={true}
+                    width='100%'
+                    placeholder='Placeholder Text'
+                    mode='html'
+                    theme='monokai'
+                    name='editor'
+                    height='100%'
+                    onChange={(newCode: string) => setCode(newCode)}
+                    fontSize={14}
+                    showPrintMargin={true}
+                    showGutter={true}
+                    highlightActiveLine={true}
+                    value={code}
+                    setOptions={{
+                        enableBasicAutocompletion: true,
+                        enableLiveAutocompletion: true,
+                        enableSnippets: true,
+                        showLineNumbers: true,
+                        tabSize: 2,
+                        wrapBehavioursEnabled: true,
+                        wrap: true,
+                    }}
+                />
+            )}
+        </>
     );
 };
 
