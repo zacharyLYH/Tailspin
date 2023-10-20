689e26a5c82f8f187b1b263fe0561304973f1563
Zac <yihongle@buffalo.edu>
2023-10-20 21:25:59 +0800

    remove full screen functionality as its not needed

--- components/core/preview.tsx
+++ components/core/preview.tsx
@@ -1,27 +1,17 @@
 "use client";
 
 import useSessionStore from "@/data-store/session-store";
-import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import useToggleFullScreen from "@/data-store/full-screen-store";
 import LandingPageCode from "../landing/test-challenges/placeholder-code";
 import { useEffect } from "react";
 import { BrowserMockup } from "@/components/ui/browserMockup";
 import Iframe from "@/components/core/iframe";
 import completeCode from "@/lib/code-complete";
 import { getSample } from "@/client-side-queries/sample-query/sample";
-import ConfettiButton from "../ui/confetti-button";
-import { handleSubmitIncrement } from "../landing/handlers/submitIncrementHandler";
+import ConfettiButton from "@/components/ui/confetti-button";
 
-import React from "react";
-
-interface EditorProps {
-    className?: string;
-}
-
-const Preview: React.FC<EditorProps> = ({ className }) => {
+const Preview = () => {
     const { code, userIframeSession, setCode } = useSessionStore();
-    const { fullScreen, toggleFullScreen } = useToggleFullScreen();
     const { isLoading, isError, data, error, refetch } = getSample();
 
     useEffect(() => {
@@ -30,19 +20,8 @@ const Preview: React.FC<EditorProps> = ({ className }) => {
         }
     }, []);
     return (
-        <div
-            className={cn(
-                "flex h-full flex-col rounded-lg",
-                fullScreen ? "col-span-3" : className
-            )}
-        >
+        <div className='flex h-full flex-col rounded-lg'>
             <div className='flex flex-row space-x-2'>
-                <Button
-                    onClick={toggleFullScreen}
-                    className='bg-purple-500 text-white'
-                >
-                    {fullScreen ? "Minimize" : "Full Screen"}
-                </Button>
                 <Button
                     onClick={() => refetch()}
                     className='bg-purple-500 text-white'
