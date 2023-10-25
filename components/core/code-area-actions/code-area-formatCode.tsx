9074fe25370b3e23d32f9aa6faadae40f4e55cb8
Zac <yihongle@buffalo.edu>
2023-10-25 08:49:26 +0800

    implement success format code button click state

--- components/core/code-area-actions/code-area-formatCode.tsx
+++ components/core/code-area-actions/code-area-formatCode.tsx
@@ -1,13 +1,32 @@
 "use client";
+
 import { Button } from "@/components/ui/button";
+import CheckMarkSuccessState from "@/components/ui/check-mark-success-state";
 import { useFormatCode } from "@/lib/codeFormatter";
+import { useState } from "react";
 
 const CodeAreaFormatCode = () => {
     const { handleFormatCode } = useFormatCode();
+    const [success, setSuccess] = useState(false);
+
+    const handleClick = async () => {
+        handleFormatCode();
+        setSuccess(true);
+        setTimeout(() => {
+            setSuccess(false);
+        }, 1000);
+    };
 
     return (
-        <Button onClick={handleFormatCode} className='bg-purple-500 text-white'>
-            Format code
+        <Button
+            onClick={handleClick}
+            className={
+                success
+                    ? "transition-background bg-green-500 text-white duration-1000"
+                    : "bg-purple-500 text-white"
+            }
+        >
+            {success ? <CheckMarkSuccessState /> : "Format code"}
         </Button>
     );
 };
