84a26598cb496f1e0bd1a0b65c008f90053c20b4
Zac <yihongle@buffalo.edu>
2023-10-27 07:22:06 +0800

    get rid of legacy image global state

--- data-store/session-store.ts
+++ data-store/session-store.ts
@@ -1,16 +1,11 @@
-// SessionStore.ts
 import { create } from "zustand";
 import React from "react";
 
 interface SessionStore {
     code: string;
     setCode: (newCode: string) => void;
-    imgPrompt: string;
-    setImgPrompt: (img: string) => void;
     userIframeSession: React.RefObject<HTMLIFrameElement>;
     promptIframeSession: React.RefObject<HTMLIFrameElement>;
-    userImg: string;
-    setUserImg: (img: string) => void;
     likenessScore: string;
     setLikenessScore: (score: string) => void;
     reset: () => void;
@@ -19,15 +14,10 @@ interface SessionStore {
 }
 
 const useSessionStore = create<SessionStore>((set) => ({
-    imgPrompt: "",
-    userImg: "",
     likenessScore: "0",
-    setUserImg: (img: string) => set({ userImg: img }),
     userIframeSession: React.createRef<HTMLIFrameElement>(),
     promptIframeSession: React.createRef<HTMLIFrameElement>(),
-    setImgPrompt: (img: string) => set({ imgPrompt: img }),
-    reset: () =>
-        set({ code: "", imgPrompt: "", userImg: "", likenessScore: "0" }),
+    reset: () => set({ code: "", likenessScore: "0" }),
     setLikenessScore: (score: string) => set({ likenessScore: score }),
     code: "",
     setCode: (newCode) => set({ code: newCode }),
