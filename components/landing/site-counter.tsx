bc9cabed81ea190dd5f9619db2bb9e0a07f6de51
Erick <ericksla@buffalo.edu>
2023-10-27 18:33:21 -0400

    I think sitecount GET is working now, have to compare to another branch

--- components/landing/site-counter.tsx
+++ components/landing/site-counter.tsx
@@ -1,24 +1,44 @@
+"use client";
+
 import {
     directDB_getSiteCount,
     directDB_getSubmitCount,
     directDB_incrementSiteCount,
 } from "@/client-side-queries/direct-db-query/landing-page-queries";
 
-import { getSiteCount } from 
+import { getSiteCount } from "@/rq-queries/site-count-queries";
+
 import { Box } from "../ui/stat-box";
 
-const SiteCounter = async () => {
-    const siteCountValue = await getSiteCount();
+const SiteCounter = () => {
+    const { isLoading, isError, isSuccess, data, error, refetch } =
+        getSiteCount();
+
+    if (isLoading) {
+        console.log("isLoading: ", isLoading);
+    }
+
+    if (isError) {
+        console.log("error is:", error);
+    }
+
+    if (isSuccess) {
+        console.log("Succesful query!");
+    }
+
+    console.log("data is: ", data?.data.fieldVal);
+
+    const siteCountValue = data?.data.fieldVal;
     //await directDB_incrementSiteCount(siteCountValue[1]!, siteCountValue[0]!);
-    const landingPageSubmitCount = await directDB_getSubmitCount();
+    //const landingPageSubmitCount = await directDB_getSubmitCount();
 
     return (
         <article className='flex  h-screen flex-col items-center gap-4 md:flex-row'>
             <Box>Site Visitors: {siteCountValue}</Box>
-            <Box>
+            {/* <Box>
                 How many people have submitted their code :{" "}
                 {landingPageSubmitCount}
-            </Box>
+            </Box> */}
         </article>
     );
 };
--- rq-queries/site-count-queries.ts
+++ rq-queries/site-count-queries.ts
@@ -1,3 +1,5 @@
+"use client";
+
 import { useQuery } from "@tanstack/react-query";
 import { SiteCountQueryKey } from "../client-side-queries/query-keys";
 import axios from "axios";
@@ -6,8 +8,9 @@ export function getSiteCount() {
     return useQuery({
         queryKey: [SiteCountQueryKey],
         queryFn: async () => {
+            console.log("queryFn of getSiteCount() called, calling axios");
             const resp = await axios.get("/api/site");
-            console.log(resp);
+            console.log("response is...", resp);
             return resp;
         },
         enabled: false,
