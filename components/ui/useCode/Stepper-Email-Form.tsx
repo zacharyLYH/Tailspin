8f40f540f330d1fdc5989e97aaa74e108a99e40c
Erick <ericksla@buffalo.edu>
2023-11-02 19:04:31 -0400

    added 2nd step functionality, but not 2nd step form/UI... This will all need some future refactoring, as it is a bit sloppy

--- components/ui/useCode/Stepper-Email-Form.tsx
+++ components/ui/useCode/Stepper-Email-Form.tsx
@@ -32,9 +32,11 @@ const formSchema = z.object({
 });
 
 export function StepperEmailForm() {
+    const PROGRESS_INCREMENT: number = 25;
     const { email, setEmail } = useStepperStore();
     const { check, setCheck } = useStepperStore();
     const { progress, setProgress } = useStepperStore();
+    const { step, setStep } = useStepperStore();
     const { setCode } = useSessionStore();
     const router = useRouter();
 
@@ -45,93 +47,196 @@ export function StepperEmailForm() {
         setCheck(values.accept);
 
         if (values.accept === true) {
-            const userEmail = values.email;
+            const userEmail: string = values.email;
             setEmail(userEmail);
             setCheck(values.accept);
             setCode(LandingPageCode());
-            router.push("/code-area");
+
+            if (step === 1) {
+                setProgress(progress + PROGRESS_INCREMENT);
+                setStep(step + 1);
+            }
+
+            if (step === 2) {
+                router.push("/code-area");
+            }
         }
     }
 
-    return (
-        <Form {...form}>
-            <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className='w-full space-y-8 p-5'
-            >
-                <FormField
-                    control={form.control}
-                    name='email'
-                    render={({ field }) => (
-                        <FormItem className='mb-[1px]'>
-                            <FormLabel>Email</FormLabel>
-                            <FormControl>
-                                <Input
-                                    placeholder='tailspin.official@gmail.com'
-                                    {...field}
-                                />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
+    async function onBack() {
+        setProgress(progress - PROGRESS_INCREMENT);
+        setStep(step - 1);
+    }
 
-                <FormField
-                    control={form.control}
-                    name='accept'
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormControl>
-                                <div className='flex'>
+    if (step === 1) {
+        return (
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className='w-full space-y-8 p-5'
+                >
+                    <FormField
+                        control={form.control}
+                        name='email'
+                        render={({ field }) => (
+                            <FormItem className='mb-[1px]'>
+                                <FormLabel>Email</FormLabel>
+                                <FormControl>
                                     <Input
-                                        type='checkbox'
-                                        checked={field.value}
-                                        onChange={field.onChange}
-                                        onClick={() => {
-                                            checked = !checked;
-                                            setCheck(checked);
-                                            setProgress(
-                                                checked
-                                                    ? progress + 25
-                                                    : progress - 25
-                                            );
-                                        }}
-                                        className='peer mr-[10px] mt-[5px] h-4 w-[20px] cursor-pointer'
+                                        placeholder='tailspin.official@gmail.com'
+                                        {...field}
                                     />
-                                    <label>
-                                        By checking the box, you are confirming
-                                        that you have read and agree to&nbsp;
-                                        {/* TODO: Replace this to link to a seperate page for Terms and Services */}
-                                        <Link
-                                            href='https://www.google.com'
-                                            className='italic text-blue-500 underline'
-                                        >
-                                            Tailspin&apos;s terms &amp;
-                                            conditions.
-                                        </Link>
-                                    </label>
-                                </div>
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
 
-                {form.formState.isSubmitting ? (
-                    <Button disabled>
-                        <Loader2 className='mr-2 h-4 w-4 animate-spin' />
-                        Submitting...
-                    </Button>
-                ) : (
-                    <Button
-                        type='submit'
-                        variant='ghost'
-                        className='self float-right font-semibold text-muted-foreground'
-                    >
-                        Continue
-                    </Button>
-                )}
-            </form>
-        </Form>
-    );
+                    <FormField
+                        control={form.control}
+                        name='accept'
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormControl>
+                                    <div className='flex'>
+                                        <Input
+                                            type='checkbox'
+                                            checked={field.value}
+                                            onChange={field.onChange}
+                                            onClick={() => {
+                                                checked = !checked;
+                                                setCheck(checked);
+                                                setProgress(
+                                                    checked
+                                                        ? progress +
+                                                              PROGRESS_INCREMENT
+                                                        : progress -
+                                                              PROGRESS_INCREMENT
+                                                );
+                                            }}
+                                            className='peer mr-[10px] mt-[5px] h-4 w-[20px] cursor-pointer'
+                                        />
+                                        <label>
+                                            By checking the box, you are
+                                            confirming that you have read and
+                                            agree to&nbsp;
+                                            {/* TODO: Replace this to link to a seperate page for Terms and Services */}
+                                            <Link
+                                                href='https://www.google.com'
+                                                className='italic text-blue-500 underline'
+                                            >
+                                                Tailspin&apos;s terms &amp;
+                                                conditions.
+                                            </Link>
+                                        </label>
+                                    </div>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    {form.formState.isSubmitting ? (
+                        <Button disabled>
+                            <Loader2 className='mr-2 h-4 w-4 animate-spin' />
+                            Submitting...
+                        </Button>
+                    ) : (
+                        <Button
+                            type='submit'
+                            variant='ghost'
+                            className='self float-right font-semibold text-muted-foreground'
+                        >
+                            Continue
+                        </Button>
+                    )}
+                </form>
+            </Form>
+        );
+    } else {
+        return (
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className='w-full space-y-8 p-5'
+                >
+                    <FormField
+                        control={form.control}
+                        name='email'
+                        render={({ field }) => (
+                            <FormItem className='mb-[1px]'>
+                                <FormLabel>Email</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        placeholder='tailspin.official@gmail.com'
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name='accept'
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormControl>
+                                    <div className='flex'>
+                                        <Input
+                                            type='checkbox'
+                                            checked={field.value}
+                                            onChange={field.onChange}
+                                            onClick={() => {
+                                                checked = !checked;
+                                                setCheck(checked);
+                                                setProgress(
+                                                    checked
+                                                        ? progress +
+                                                              PROGRESS_INCREMENT
+                                                        : progress -
+                                                              PROGRESS_INCREMENT
+                                                );
+                                            }}
+                                            className='peer mr-[10px] mt-[5px] h-4 w-[20px] cursor-pointer'
+                                        />
+                                        <label>
+                                            By checking the box, you are
+                                            confirming that you have read and
+                                            agree to&nbsp;
+                                            {/* TODO: Replace this to link to a seperate page for Terms and Services */}
+                                            <Link
+                                                href='https://www.google.com'
+                                                className='italic text-blue-500 underline'
+                                            >
+                                                Tailspin&apos;s terms &amp;
+                                                conditions.
+                                            </Link>
+                                        </label>
+                                    </div>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    {form.formState.isSubmitting ? (
+                        <Button disabled>
+                            <Loader2 className='mr-2 h-4 w-4 animate-spin' />
+                            Submitting...
+                        </Button>
+                    ) : (
+                        <Button
+                            type='submit'
+                            variant='ghost'
+                            className='self float-right font-semibold text-muted-foreground'
+                        >
+                            Continue
+                        </Button>
+                    )}
+                </form>
+            </Form>
+        );
+    }
 }
