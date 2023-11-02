6b9a1da10a521e3d3495d747cdbf00f1ad533c5f
Erick <ericksla@buffalo.edu>
2023-11-01 23:18:05 -0400

    got form step 1 to work, with error messages, email, and TOS.

--- components/ui/useCode/Stepper-Email-Form.tsx
+++ components/ui/useCode/Stepper-Email-Form.tsx
@@ -15,35 +15,55 @@ import {
 import { Input } from "@/components/ui/input";
 import { Loader2 } from "lucide-react";
 import useStepperStore from "@/data-store/stepper-store";
-import TermsOfServiceBox from "./Terms-of-Service";
+import Link from "next/link";
+import useSessionStore from "@/data-store/session-store";
+import { useRouter } from "next/navigation";
+import LandingPageCode from "@/components/landing/test-challenges/placeholder-code";
+
+let checked: boolean = false;
 
 const formSchema = z.object({
     email: z.string().email({
         message: "Invalid email address.",
+    }),
+    accept: z.literal<boolean>(true, {
+        errorMap: () => ({ message: "You must accept the terms & conditions" }),
     }),
 });
 
 export function StepperEmailForm() {
     const { email, setEmail } = useStepperStore();
     const { check, setCheck } = useStepperStore();
+    const { progress, setProgress } = useStepperStore();
+    const { setCode } = useSessionStore();
+    const router = useRouter();
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
     });
     async function onSubmit(values: z.infer<typeof formSchema>) {
-        const userEmail = JSON.stringify(values.email);
-        console.log("user email is", userEmail);
-        setEmail(userEmail);
-        console.log(email);
+        setCheck(values.accept);
+
+        if (values.accept === true) {
+            const userEmail = values.email;
+            setEmail(userEmail);
+            setCheck(values.accept);
+            setCode(LandingPageCode());
+            router.push("/code-area");
+        }
     }
+
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+            <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className='w-full space-y-8 p-5'
+            >
                 <FormField
                     control={form.control}
                     name='email'
                     render={({ field }) => (
-                        <FormItem>
+                        <FormItem className='mb-[1px]'>
                             <FormLabel>Email</FormLabel>
                             <FormControl>
                                 <Input
@@ -56,7 +76,46 @@ export function StepperEmailForm() {
                     )}
                 />
 
-                <TermsOfServiceBox />
+                <FormField
+                    control={form.control}
+                    name='accept'
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormControl>
+                                <div className='flex'>
+                                    <Input
+                                        type='checkbox'
+                                        checked={field.value}
+                                        onChange={field.onChange}
+                                        onClick={() => {
+                                            checked = !checked;
+                                            setCheck(checked);
+                                            setProgress(
+                                                checked
+                                                    ? progress + 25
+                                                    : progress - 25
+                                            );
+                                        }}
+                                        className='peer mr-[10px] mt-[5px] h-4 w-[20px] cursor-pointer'
+                                    />
+                                    <label>
+                                        By checking the box, you are confirming
+                                        that you have read and agree to&nbsp;
+                                        {/* TODO: Replace this to link to a seperate page for Terms and Services */}
+                                        <Link
+                                            href='https://www.google.com'
+                                            className='italic text-blue-500 underline'
+                                        >
+                                            Tailspin&apos;s terms &amp;
+                                            conditions.
+                                        </Link>
+                                    </label>
+                                </div>
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
 
                 {form.formState.isSubmitting ? (
                     <Button disabled>
