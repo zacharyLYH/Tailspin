f794062106941b90da321261e33b83ec02ab9e60
Zac <yihongle@buffalo.edu>
2023-11-05 18:11:01 +0800

    rename file

---
+++ components/landing/stats.tsx
@@ -0,0 +1,20 @@
+import { CodeSubmitCountBox } from "../ui/statistics/code-submit-count-box";
+import { SiteRatingStatBox } from "../ui/statistics/site-rating";
+import { SiteVisitCountBox } from "../ui/statistics/site-visit-count-box";
+
+const SiteCounter = async () => {
+    return (
+        <div className='mx-12 flex h-screen flex-col items-center justify-center'>
+            <h1 className='mb-5 text-center text-3xl font-bold italic'>
+                Some Statistics
+            </h1>
+            <article className=' grid gap-6 md:grid-cols-2'>
+                <SiteVisitCountBox />
+                <CodeSubmitCountBox />
+                <SiteRatingStatBox />
+            </article>
+        </div>
+    );
+};
+
+export default SiteCounter;
