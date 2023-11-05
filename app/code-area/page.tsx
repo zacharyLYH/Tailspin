38a39fd0e6f9879000e7e8bc12fd60a220563e23
Erick <ericksla@buffalo.edu>
2023-11-05 01:10:43 -0500

    added basic error if bypassing to the code area, needs to be turned into a proper alert/error message

--- app/code-area/page.tsx
+++ app/code-area/page.tsx
@@ -9,6 +9,7 @@ import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 import useCodeAreaStore from "@/data-store/code-area-store";
+import useStepperStore from "@/data-store/stepper-store";
 
 type MosaicKey = "EDITOR" | "PREVIEW";
 
@@ -25,69 +26,86 @@ const CodeArea = () => {
     );
     const router = useRouter();
     const { mosaicThemeDark } = useCodeAreaStore();
+    const { email } = useStepperStore();
+    const { check } = useStepperStore();
+    const { progress } = useStepperStore();
+    const { challenge } = useStepperStore();
 
-    return (
-        <div className='h-screen'>
-            <div className='pointer-events-auto absolute inset-0 z-20 bg-white bg-opacity-50 xl:hidden' />
-            <div className='absolute inset-0 z-30 flex items-center justify-center xl:hidden'>
-                <div className='w-3/4 max-w-lg rounded-xl bg-black p-6 shadow-lg'>
-                    <h2 className='mb-4 text-center text-xl font-bold text-white'>
-                        It looks like you&quot;re on a small screen
-                    </h2>
-                    <p className='text-sm font-semibold text-gray-400'>
-                        We hate to be non-inclusive towards phones and tablets,
-                        however we want to provide you with the best experience
-                        possible!
-                    </p>
-                    <div className='flex justify-center'>
-                        <Button
-                            variant='destructive'
-                            className='mt-2'
-                            onClick={() => router.push("/")}
-                        >
-                            Back to main page...
-                        </Button>
+    if (
+        email === "" &&
+        check === false &&
+        progress !== (99 || 100) &&
+        challenge === ""
+    ) {
+        return (
+            <p>
+                error, no bypassing the required form to enter the coding area!
+            </p>
+        );
+    } else {
+        return (
+            <div className='h-screen'>
+                <div className='pointer-events-auto absolute inset-0 z-20 bg-white bg-opacity-50 xl:hidden' />
+                <div className='absolute inset-0 z-30 flex items-center justify-center xl:hidden'>
+                    <div className='w-3/4 max-w-lg rounded-xl bg-black p-6 shadow-lg'>
+                        <h2 className='mb-4 text-center text-xl font-bold text-white'>
+                            It looks like you&quot;re on a small screen
+                        </h2>
+                        <p className='text-sm font-semibold text-gray-400'>
+                            We hate to be non-inclusive towards phones and
+                            tablets, however we want to provide you with the
+                            best experience possible!
+                        </p>
+                        <div className='flex justify-center'>
+                            <Button
+                                variant='destructive'
+                                className='mt-2'
+                                onClick={() => router.push("/")}
+                            >
+                                Back to main page...
+                            </Button>
+                        </div>
                     </div>
                 </div>
+                <Mosaic<MosaicKey>
+                    className={cn(
+                        "mosaic-blueprint-theme",
+                        mosaicThemeDark && "bp4-dark"
+                    )}
+                    value={layout || initialLayout}
+                    onChange={(newLayout) => setLayout(newLayout)}
+                    renderTile={(id, path) => {
+                        switch (id) {
+                            case "EDITOR":
+                                return (
+                                    <MosaicWindow<MosaicKey>
+                                        title='Editor'
+                                        path={path}
+                                    >
+                                        <>
+                                            <Editor />
+                                        </>
+                                    </MosaicWindow>
+                                );
+                            case "PREVIEW":
+                                return (
+                                    <MosaicWindow<MosaicKey>
+                                        title='Preview'
+                                        path={path}
+                                    >
+                                        <>
+                                            <Preview />
+                                        </>
+                                    </MosaicWindow>
+                                );
+                            default:
+                                return <></>; // Return a React Fragment as a fallback
+                        }
+                    }}
+                />
             </div>
-            <Mosaic<MosaicKey>
-                className={cn(
-                    "mosaic-blueprint-theme",
-                    mosaicThemeDark && "bp4-dark"
-                )}
-                value={layout || initialLayout}
-                onChange={(newLayout) => setLayout(newLayout)}
-                renderTile={(id, path) => {
-                    switch (id) {
-                        case "EDITOR":
-                            return (
-                                <MosaicWindow<MosaicKey>
-                                    title='Editor'
-                                    path={path}
-                                >
-                                    <>
-                                        <Editor />
-                                    </>
-                                </MosaicWindow>
-                            );
-                        case "PREVIEW":
-                            return (
-                                <MosaicWindow<MosaicKey>
-                                    title='Preview'
-                                    path={path}
-                                >
-                                    <>
-                                        <Preview />
-                                    </>
-                                </MosaicWindow>
-                            );
-                        default:
-                            return <></>; // Return a React Fragment as a fallback
-                    }
-                }}
-            />
-        </div>
-    );
+        );
+    }
 };
 
 export default CodeArea;
