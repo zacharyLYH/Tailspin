2dba340bcbb1c71b7974c05a93aa8b4688c702cb
Zac <yihongle@buffalo.edu>
2023-11-05 18:11:16 +0800

    rename Box component

--- components/ui/stat-box.tsx
+++ components/ui/stat-box.tsx
@@ -1,8 +1,15 @@
-export const Box = (props: any) => {
+interface StatBoxProps {
+    icon: React.ReactNode;
+    content: string;
+}
+
+const StatBox: React.FC<StatBoxProps> = ({ icon, content }) => {
     return (
-        <div className='m:h-[500px] m:w-[600px] mx-auto mt-20 flex h-[400px] w-[400px] items-center justify-center rounded-lg bg-slate-800 object-center p-4'>
-            {"   "}
-            {props.children}{" "}
+        <div className='flex flex-col items-center text-center'>
+            {icon}
+            {content}
         </div>
     );
 };
+
+export default StatBox;
