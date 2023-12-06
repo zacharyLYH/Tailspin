0677f0a5e323afb8d1463813ef11df4108ffe54c
Zac <yihongle@buffalo.edu>
2023-12-06 13:13:18 +0800

    add more detail

--- components/landing/challenge/challenge-intro.tsx
+++ components/landing/challenge/challenge-intro.tsx
@@ -19,11 +19,14 @@ const ChallengeIntro: React.FC<ChallengeIntroProps> = ({ button }) => {
         <Card className='mx-auto my-8 h-[420px] w-2/3 rounded-2xl bg-gray-800'>
             <CardHeader className='p-4'>
                 <CardTitle className='mb-4 text-3xl font-semibold text-white'>
-                    We&apos;re glad you&apos;re here!
+                    🥳 We&apos;re glad you&apos;re here!
                 </CardTitle>
                 <CardDescription>
-                    <Badge className='rounded-full bg-blue-400 px-2 py-1 text-white'>
+                    <Badge className='mx-1 rounded-full bg-blue-400 px-2 py-1 text-white'>
                         Alpha
+                    </Badge>
+                    <Badge className='mx-1 rounded-full bg-blue-400 px-2 py-1 text-white'>
+                        MVP
                     </Badge>
                 </CardDescription>
             </CardHeader>
@@ -31,8 +34,8 @@ const ChallengeIntro: React.FC<ChallengeIntroProps> = ({ button }) => {
             <CardContent className='p-4'>
                 <p className='text-gray-300'>
                     It&apos;s simple. We&apos;ll provide you a coding
-                    environment and a target image. Your job is to recreate that
-                    image using{" "}
+                    environment and a target image 🖼️. Your job is to recreate
+                    that image using{" "}
                     <span className='font-semibold text-green-500'>HTML</span>{" "}
                     and{" "}
                     <span className='font-semibold text-green-500'>
@@ -43,9 +46,12 @@ const ChallengeIntro: React.FC<ChallengeIntroProps> = ({ button }) => {
                     <br /> Currently, we only provide a playground which
                     represents the core services of Tailspin. When you submit
                     code from our platform, we score your code and email the
-                    result of similarity to you. We don&apos;t have a way for
-                    you to track all your previous submissions or rank yourself
-                    against other developers trying out Tailspin.
+                    result of similarity to you.{" "}
+                    <span className='text-red-500'>
+                        In this MVP, we don&apos;t have a way for you to track
+                        all your previous submissions or rank yourself against
+                        other developers trying out Tailspin.
+                    </span>
                 </p>
             </CardContent>
             <CardFooter className='p-4'>
