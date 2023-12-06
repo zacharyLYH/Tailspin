a6d928336ca4b01c2b8f8aa39ada1f0a3b2318d9
Zac <yihongle@buffalo.edu>
2023-12-06 11:37:26 +0800

    add return types

--- lib/localStorage.ts
+++ lib/localStorage.ts
@@ -1,4 +1,4 @@
-export const saveToLocalStorage = (key: string, value: string) => {
+export const saveToLocalStorage = (key: string, value: string): void => {
     try {
         if (typeof window !== "undefined") {
             localStorage.setItem(key, value);
@@ -8,22 +8,23 @@ export const saveToLocalStorage = (key: string, value: string) => {
     }
 };
 
-export const loadFromLocalStorage = (key: string) => {
+export const loadFromLocalStorage = (key: string): string => {
     try {
         if (typeof window !== "undefined") {
             const value = localStorage.getItem(key);
-            if (value === null) {
+            if (value === null || value === undefined) {
                 return "";
             }
             return value;
         }
+        return "";
     } catch (error) {
         console.error("Error reading from localStorage", error);
         return "";
     }
 };
 
-export const removeItemFromLocalStorage = (key: string) => {
+export const removeItemFromLocalStorage = (key: string): void => {
     if (typeof window !== "undefined") {
         localStorage.removeItem(key);
     }
