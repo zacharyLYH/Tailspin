4c2627ef994a30635ad6eea5e00dad626121fe94
Zac <yihongle@buffalo.edu>
2023-12-23 08:36:36 +0800

    Implement async await in nodemailer implementation

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -7,16 +7,31 @@ export async function GET() {
     try {
         console.log("ENTERED TEST EMAIL");
         const transporter = nodemailer.createTransport({
-            service: "gmail",
+            port: 465,
+            host: "smtp.gmail.com",
+            // service: "gmail",
             auth: {
                 user: process.env.EMAIL,
                 pass: process.env.PASSWORD,
             },
+            secure: true,
+        });
+        await new Promise((resolve, reject) => {
+            // verify connection configuration
+            transporter.verify(function (error, success) {
+                if (error) {
+                    console.log(error);
+                    reject(error);
+                } else {
+                    console.log("Server is ready to take our messages");
+                    resolve(success);
+                }
+            });
         });
         console.log("CREATED TRANSPORTER");
         const emailHtml = render(
             FeedbackEmail({
-                name: "TESTING01",
+                name: "TESTING02",
                 email: "asdf",
                 message: "Testing email in production",
             })
@@ -28,7 +43,18 @@ export async function GET() {
             html: emailHtml,
         };
         console.log("CREATED OPTIONS");
-        transporter.sendMail(options);
+        await new Promise((resolve, reject) => {
+            // send mail
+            transporter.sendMail(options, (err, info) => {
+                if (err) {
+                    console.error(err);
+                    reject(err);
+                } else {
+                    console.log(info);
+                    resolve(info);
+                }
+            });
+        });
         console.log("SENT EMAIL");
         return NextResponse.json({ message: "Success" }, { status: 200 });
     } catch (error) {
